9971469fd51cf22e43cb2f67e34fef235f7c0e6a
ionutvidrighin <ionut.vidrighin@gmail.com>
2021-03-07 17:51:37 +0200

    fixing menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './Nav.css';
+import MenuOpen from './pics/open.png';
+import MenuClose from './pics/close.png';
 import TikTokLogo from './pics/tiktok.png';
-import Logo from './pics/ionut_v.png';
+import MyLogo from './pics/ionut_v.png';
 
 function Nav({menu, setMenu}) {
 
-
-
     return (
         <div className="nav">
-            <div onClick={() => setMenu(!menu)}>
-                <div className="line"></div>
-                <div className="line"></div>
-                <div className="line"></div>
+            <div className="menu" onClick={() => setMenu(!menu)}>
+                <img src={ menu ? MenuClose : MenuOpen} alt="menu"/>
             </div>
             
             <div>
@@ -20,7 +18,7 @@ function Nav({menu, setMenu}) {
             </div>
 
             <div>
-                <img className="logo" src={Logo} alt="logo"/>
+                <img className="logo" src={MyLogo} alt="logo"/>
             </div>
         </div>
     )
